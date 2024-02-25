ec83453cb7ee5cf750dee4153ca98ff4d1c19fe2
SnehaaSinghh9 <101444040+SnehaaSinghh9@users.noreply.github.com>
2024-02-25 23:58:31 +0530

    2709 - Hard level

--- feb25.py
+++ feb25.py
@@ -6,37 +6,22 @@
 #Return true if it is possible to traverse between all such pairs of indices, or false otherwise.
 
 class Solution:
-    def dfs(self, index, visitedIndex, visitedPrime):
-        if visitedIndex[index]:
-            return
-        visitedIndex[index] = True
-
-        for prime in self.index2prime[index]:
-            if visitedPrime.get(prime, False):
-                continue
-            visitedPrime[prime] = True
-            for index1 in self.prime2index[prime]:
-                if visitedIndex[index1]:
-                    continue
-                self.dfs(index1, visitedIndex, visitedPrime)
-
     def canTraverseAllPairs(self, nums: List[int]) -> bool:
-        self.prime2index = {}
-        self.index2prime = {}
-        for i, num in enumerate(nums):
-            temp = num
-            for j in range(2, int(num ** 0.5) + 1):
-                if temp % j == 0:
-                    self.prime2index.setdefault(j, []).append(i)
-                    self.index2prime.setdefault(i, []).append(j)
-                    while temp % j == 0:
-                        temp //= j
-            if temp > 1:
-                self.prime2index.setdefault(temp, []).append(i)
-                self.index2prime.setdefault(i, []).append(temp)
+        if len(nums)==1:
+            return True
+        nums = set(nums)
+        if 1 in nums:
+            return False
+        if len(nums)==1:
+            return True
+        
+        nums = sorted(nums,reverse=True)
 
-        visitedIndex = [False] * len(nums)
-        visitedPrime = {}
-        self.dfs(0, visitedIndex, visitedPrime)
-
-        return all(visitedIndex)
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if gcd(nums[i],nums[j])-1:
+                    nums[j]*=nums[i]
+                    break
+            else:
+                return False
+        return True
